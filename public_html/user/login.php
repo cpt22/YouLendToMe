aa9c90d938c04b17f9bd91ab896aae11c472a83c
Christian <ctingle18@gmail.com>
2020-02-07 17:53:40 -0500

    Templates for navbar setup

--- public_html/user/login.php
+++ public_html/user/login.php
@@ -1,2 +1,11 @@
-<?php
-?>
+<!doctype html>
+<html lang="en">
+<head>
+<?php require_once('../includes/header.php'); ?>
+<title>You Lend To Me</title>
+</head>
+<body>
+	<?php require_once('../includes/navbar.php'); ?>
+	<?php require_once('../includes/footer.php'); ?>
+</body>
+</html>
