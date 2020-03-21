4b3e1d6920657bc4aac63cb9e754661c1b91675e
Christian <ctingle18@gmail.com>
2020-03-21 17:24:56 -0500

    test of changing constants

--- html/index.php
+++ html/index.php
@@ -1,13 +1,13 @@
-<?php define('__ROOT__', dirname(dirname(__FILE__)));
-require_once __ROOT__ . '/includes/session.php' ?>
+<?php 
+require_once ROOT . '/includes/session.php' ?>
 <!doctype html>
 <html lang="en">
 <head>
-<?php require_once __ROOT__ . '/includes/components/header.php'; ?>
+<?php require_once ROOT . '/includes/components/header.php'; ?>
 <title>You Lend To Me</title>
 </head>
 <body>
-	<?php require_once __ROOT__ . '/includes/components/navbar.php'; ?>
-	<?php require_once __ROOT__ . '/includes/components/footer.php'; ?>
+	<?php require_once ROOT . '/includes/components/navbar.php'; ?>
+	<?php require_once ROOT . '/includes/components/footer.php'; ?>
 </body>
 </html>
