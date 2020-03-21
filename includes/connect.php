02d46db61a436f670caed8c628ecc9bd20e862c3
Christian <ctingle18@gmail.com>
2020-03-21 09:55:46 -0500

    updated creds

--- includes/connect.php
+++ includes/connect.php
@@ -1,3 +1,3 @@
 <?php
-$con = mysqli_connect('localhost', 'yltmaccess', '^5P;biD`Zw!%\m74', 'yltm') or die(mysqli_error($con));
+$con = mysqli_connect('localhost', 'yltmaccess', 'a3MG7XpdUcK6fFoF', 'yltm') or die(mysqli_error($con));
 ?>
