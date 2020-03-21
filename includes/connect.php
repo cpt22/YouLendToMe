2c16d4510713a6842ede1264a5b04f4aba045cd9
Christian <ctingle18@gmail.com>
2020-03-21 09:41:26 -0500

    updated mysql username and password

--- includes/connect.php
+++ includes/connect.php
@@ -1,3 +1,3 @@
 <?php
-$con = mysqli_connect('localhost', 'root', 'mysql', 'yltm') or die(mysqli_error($con));
+$con = mysqli_connect('localhost', 'yltmaccess', '^5P;biD`Zw!%\m74', 'yltm') or die(mysqli_error($con));
 ?>
