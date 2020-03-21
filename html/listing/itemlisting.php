d837cb66d7a6e4921dc0380ee8e3c791b4bda40d
Christian <ctingle18@gmail.com>
2020-03-21 18:31:54 -0500

    created

---
+++ html/listing/itemlisting.php
@@ -0,0 +1,14 @@
+<?php
+require_once SRC . 'session.php' ?>
+<!doctype html>
+<html lang="en">
+<head>
+<?php require_once SRC . 'components/header.php'; ?>
+<title>You Lend To Me</title>
+</head>
+<body>
+	<?php require_once SRC . 'components/navbar.php'; ?>
+	<?php require_once SRC . 'components/footer.php'; ?>
+</body>
+</html>
+
