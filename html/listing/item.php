01ade8ab33d2d21a8169977ed583ced1738ebff3
orionfollett <60243710+orionfollett@users.noreply.github.com>
2020-03-23 14:46:20 -0700

    Finished Item view page, I left redborder around image so we can see where the image will be, feel free to delete the border

--- html/listing/item.php
+++ html/listing/item.php
@@ -18,11 +18,23 @@ if (isset($_GET['i'])) {
 <head>
 <?php require_once SRC . 'components/header.php'; ?>
 
+
+
 <style>
 
-.testBorder{
-bprder: 1px solid black
 
+.blackBorder{
+border: 1px solid black
+}
+
+
+.redBorder{
+border: 1px solid red
+}
+
+
+.greenBorder{
+border: 1px solid green
 }
 
 </style>
@@ -32,57 +44,27 @@ bprder: 1px solid black
 <body>
 	<?php require_once SRC . 'components/navbar.php'; ?>
 
-<!--
-  <div class="container">
-    <div class = "row">
-      <div class = "col">
-        Image here
-        <img src="image link here" class="rounded float-left" alt="image alternate text here" width="500" height="600">
-      </div>
-      <div class = "col">
-          <h1 class="itemName">Item Name PlaceHolder</h1>
-      </div>
-     </div>
 
-      <div class = "row">
-        <div class = "col"></div>
-        <div class = "col">
-            Available from MM/XX/YYY to MM/XX/YYYY
-        </div>
-      </div>
-        <div class = "row">
-          <div class = "col">
-          </div>
-          <div class = "col">
+  <div class="container p-3">
+  <div class="row">
+  <div class="col redBorder"><img src="image link here" class="rounded float-left" alt="image alternate text here" width="400" height="400"></div>
 
-          </div>
-      </div>
+  <div class="col">
+  <div class="row p-2"><h1 class="itemName">Item Name PlaceHolder</h1></div>
+  <div class="row p-2">Dates</div>
+  <div class="row p-2">Price</div>
+  <div class="row p-2">Rent now</div>
   </div>
--->
+  </div>
+  </div>
 
-  <div class="container testBorder">
-<div class="col">Image here <img src="image link here" class="rounded float-left" alt="image alternate text here" width="400" height="500"></div>
-</div>
 
-  <div class="container">
+  <div class="container blackBorder">
     <div class="row">
-      <div class="col offset-md-3"><h1 class="itemName">Item Name PlaceHolder</h1></div>
-      <div class="w-100"></div>
-
-      <div class="col offset-md-3">Available from MM/XX/YYY to MM/XX/YYYY</div>
+    <div class="col">DESCRIPTION: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+       nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+        deserunt mollit anim id est laborum.</div>
     </div>
-
-    <div class="row">
-      <div class="col offset-md-3">Daily Price: $10</div>
-      <div class="w-100"></div>
-      <div class="col offset-md-3">Item Category:</div>
-    </div>
-
-
-        <div class="row">
-          <div class="col">DESCRIPTION</div>
-        </div>
-
   </div>
 
 
