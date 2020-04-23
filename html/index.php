d08bc6bfd98541948dc6732d18eb914d245263e2
Prithik Karthikeyan <prithik2000@gmail.com>
2020-04-23 19:53:56 -0400

    commit

--- html/index.php
+++ html/index.php
@@ -18,12 +18,7 @@
 
 		<div class="col-lg-3">
 
-			<h1 class="my-4">Shop Name</h1>
-			<div class="list-group">
-				<a href="#" class="list-group-item">Electronics</a>
-				<a href="#" class="list-group-item">Sports</a>
-				<a href="#" class="list-group-item">Music</a>
-			</div>
+		
 
 		</div>
 		<!-- /.col-lg-3 -->
