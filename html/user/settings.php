ba0709ed25778141d19a25b6200e2ff992278be0
Christian <ctingle18@gmail.com>
2020-04-26 12:45:02 -0500

    wut

--- html/user/settings.php
+++ html/user/settings.php
@@ -1,5 +1,5 @@
 <?php
-if (!isUserLoggedIn()) {
+if (! isUserLoggedIn()) {
     sendToLogin(null);
 }
 require_once SRC . 'user/updateAccountInfo.php';
@@ -14,13 +14,14 @@ require_once SRC . 'user/updateAccountInfo.php';
 	<?php require_once SRC . 'components/navbar.php'; ?>
 	<?php require_once SRC . 'components/footer.php'; ?>
   <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-12">Account Information</h1>
-    <p class="lead">View/Change your account details</p>
-  </div>
+		<div class="container">
+			<h1 class="display-12">Account Information</h1>
+			<p class="lead">View/Change your account details</p>
+		</div>
+	</div>
 
 
-  <div class="container">
+	<div class="container">
 		<div class="row">
 			<div class="col-9 col-sm-6">
 				<div class="form-signin">
@@ -29,98 +30,77 @@ require_once SRC . 'user/updateAccountInfo.php';
 							enctype="multipart/form-data">
 							<div class="form-group">
 								<label for="itemTitle">Email Address</label> <input type="text"
-									class="<?php echo isset($errors['itemTitle']) ? "is-invalid" : ""; ?> form-control"
-									id="itemTitle" placeholder="Email" name="itemTitle"
-									<?php echo isset($vals['itemTitle']) ? 'value="'.$vals['itemTitle'].'"' : ""; ?>
-									required autofocus>
+									class="form-control" id="itemTitle" placeholder="Email"
+									name="itemTitle" required autofocus>
 							</div>
 
-              <div class="form-group">
+							<div class="form-group">
 								<label for="itemTitle">Phone Number</label> <input type="text"
-									class="<?php echo isset($errors['itemTitle']) ? "is-invalid" : ""; ?> form-control"
-									id="itemTitle" placeholder="Phone Number" name="itemTitle"
-									<?php echo isset($vals['itemTitle']) ? 'value="'.$vals['itemTitle'].'"' : ""; ?>
-									required autofocus>
+									class="form-control" id="itemTitle" placeholder="Phone Number"
+									name="itemTitle" required autofocus>
 							</div>
 
-              <div class="form-group">
-								<label for="itemTitle">Address</label>
+							<div class="form-group">
+								<label for="itemTitle">Address</label> <input type="text"
+									class="form-control" id="itemTitle"
+									placeholder="Address Line 1" name="itemTitle" required
+									autofocus> <input type="text" class="form-control"
+									id="itemTitle" placeholder="Address Line 2" name="itemTitle"
+									required autofocus> <input type="text" class="form-control"
+									id="itemTitle" placeholder="City" name="itemTitle" required
+									autofocus>
 
-                <input type="text"
-									class="<?php echo isset($errors['itemTitle']) ? "is-invalid" : ""; ?> form-control"
-									id="itemTitle" placeholder="Address Line 1" name="itemTitle"
-									<?php echo isset($vals['itemTitle']) ? 'value="'.$vals['itemTitle'].'"' : ""; ?>
-									required autofocus>
-                <input type="text"
-                    class="<?php echo isset($errors['itemTitle']) ? "is-invalid" : ""; ?> form-control"
-                    id="itemTitle" placeholder="Address Line 2" name="itemTitle"
-                    <?php echo isset($vals['itemTitle']) ? 'value="'.$vals['itemTitle'].'"' : ""; ?>
-                    required autofocus>
-                <input type="text"
-                    class="<?php echo isset($errors['itemTitle']) ? "is-invalid" : ""; ?> form-control"
-                    id="itemTitle" placeholder="City" name="itemTitle"
-                    <?php echo isset($vals['itemTitle']) ? 'value="'.$vals['itemTitle'].'"' : ""; ?>
-                    required autofocus>
-
-                <div class="form-label-group">
-									<select id="inputState" class="<?php echo isset($errors['state']) ? "is-invalid" : ""; ?> form-control form-control-lg" name="state"
-										<?php echo isset($vals['state']) ? 'value="'.$vals['state'].'"' : ""; ?> required>
+								<div class="form-label-group">
+									<select id="inputState"
+										class="<?php echo isset($errors['state']) ? "is-invalid" : ""; ?> form-control form-control-lg"
+										name="state"
+										<?php echo isset($vals['state']) ? 'value="'.$vals['state'].'"' : ""; ?>
+										required>
 										<option>State</option>
 										<?php
-										global $USStates;
-										foreach ($USStates as $thisState) {
-										    echo '<option value="' . $thisState . '">' . $thisState . '</option>';
-										}
-										?>
-									</select>
-									<span class="error_form" id="inputState_error"></span>
+        global $USStates;
+        foreach ($USStates as $thisState) {
+            echo '<option value="' . $thisState . '">' . $thisState . '</option>';
+        }
+        ?>
+									</select> <span class="error_form" id="inputState_error"></span>
 								</div>
 
-                <input type="text"
-                    class="<?php echo isset($errors['itemTitle']) ? "is-invalid" : ""; ?> form-control"
-                    id="itemTitle" placeholder="Zipcode" name="itemTitle"
-                    <?php echo isset($vals['itemTitle']) ? 'value="'.$vals['itemTitle'].'"' : ""; ?>
-                    required autofocus>
+								<input type="text" class="form-control" id="itemTitle"
+									placeholder="Zipcode" name="itemTitle" required autofocus>
 
-                </div>
+							</div>
 
-                <div class="form-group">
-                  <label for="itemTitle">Credit Card Information</label>
+							<div class="form-group">
+								<label for="itemTitle">Credit Card Information</label> <input
+									type="text" class="form-control" id="itemTitle"
+									placeholder="Credit Card Number" name="itemTitle" required
+									autofocus>
 
-                  <input type="text"
-                    class="<?php echo isset($errors['itemTitle']) ? "is-invalid" : ""; ?> form-control"
-                    id="itemTitle" placeholder="Credit Card Number" name="itemTitle"
-                    <?php echo isset($vals['itemTitle']) ? 'value="'.$vals['itemTitle'].'"' : ""; ?>
-                    required autofocus>
-
-                    <div class="form-label-group">
-    									<select id="inputState" class="<?php echo isset($errors['state']) ? "is-invalid" : ""; ?> form-control form-control-lg" name="state"
-    										<?php echo isset($vals['state']) ? 'value="'.$vals['state'].'"' : ""; ?> required>
-    										<option>Expiration Month</option>
+								<div class="form-label-group">
+									<select id="inputState"
+										class="<?php echo isset($errors['state']) ? "is-invalid" : ""; ?> form-control form-control-lg"
+										name="state"
+										<?php echo isset($vals['state']) ? 'value="'.$vals['state'].'"' : ""; ?>
+										required>
+										<option>Expiration Month</option>
     										<?php
-    										global $Months;
-    										foreach ($Months as $thisState) {
-    										    echo '<option value="' . $thisState . '">' . $thisState . '</option>';
-    										}
-    										?>
-    									</select>
-    									<span class="error_form" id="inputState_error"></span>
-    								</div>
+            global $Months;
+            foreach ($Months as $thisState) {
+                echo '<option value="' . $thisState . '">' . $thisState . '</option>';
+            }
+            ?>
+    									</select> <span class="error_form" id="inputState_error"></span>
+								</div>
 
-                  <input type="text"
-                    class="<?php echo isset($errors['itemTitle']) ? "is-invalid" : ""; ?> form-control"
-                    id="itemTitle" placeholder="Expiration Year" name="itemTitle"
-                    <?php echo isset($vals['itemTitle']) ? 'value="'.$vals['itemTitle'].'"' : ""; ?>
-                    required autofocus>
-
-                  <input type="text"
-                    class="<?php echo isset($errors['itemTitle']) ? "is-invalid" : ""; ?> form-control"
-                    id="itemTitle" placeholder="CVV" name="itemTitle"
-                    <?php echo isset($vals['itemTitle']) ? 'value="'.$vals['itemTitle'].'"' : ""; ?>
-                    required autofocus>
+								<input type="text" class="form-control" id="itemTitle"
+									placeholder="Expiration Year" name="itemTitle" required
+									autofocus> <input type="text" class="form-control"
+									id="itemTitle" placeholder="CVV" name="itemTitle" required
+									autofocus>
 
 
-                </div>
+							</div>
 
 
 
@@ -128,7 +108,8 @@ require_once SRC . 'user/updateAccountInfo.php';
 
 
 							<div class="form-group">
-								<button type="submit" class="btn btn-primary btn-lg">Save Information</button>
+								<button type="submit" class="btn btn-primary btn-lg">Save
+									Information</button>
 							</div>
 						</form>
 					</div>
@@ -136,8 +117,6 @@ require_once SRC . 'user/updateAccountInfo.php';
 			</div>
 		</div>
 	</div>
-
-
 
 </body>
 </html>
